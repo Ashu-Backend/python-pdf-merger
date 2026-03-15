48b5a426d728c5096c342597f351e10afe84fae9
Ashu-Backend <151012015+Ashu-Backend@users.noreply.github.com>
2026-03-15 18:24:21 +0530

    Refactor pdf_merger.py to remove unused code

--- pdf_merger.py
+++ pdf_merger.py
@@ -10,26 +10,4 @@ for pdf in files:
 merger.write("merged-pdf.pdf")
 merger.close()
 
-
-
-import string
-import random
-def missingCharacters(s):
-    s = set(s.lower())
-    
-    digits = ""
-    letters = ""
-    
-    for d in string.digits:
-        if d not in s:
-            digits += d
-            
-    for l in string.ascii_lowercase:
-        if l not in s:
-            letters += l
-            
-    return digits + letters
-    
-s = "0123456789"
-print(missingCharacters(s))
     
